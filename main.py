7a4392166146b73de7a3b3e87edee91263daf0c0
Michelle Schuster <Michelle.Schuster@sjrb.ca>
2013-10-27 17:02:08 -0600

    added couple of links for the main and details page

--- main.py
+++ main.py
@@ -3,21 +3,53 @@
 
 import os
 from google.appengine.ext import webapp
-from google.appengine.ext.webapp import util
+# from google.appengine.ext.webapp import util
 from google.appengine.ext.webapp import template
 
 class MainHandler(webapp.RequestHandler):
-  def get (self, q):
-    if q is None:
-      q = 'index'
+	def get (self, q):
 
-    path = os.path.join (os.path.dirname (__file__), q + '.html')
-    self.response.headers ['Content-Type'] = 'text/html'
-    self.response.out.write (template.render (path, {}))
+		if not q:
+			q = 'index'
+		# if "." not in q:
+		# 	q = q + '.html'
 
-def main ():
-  application = webapp.WSGIApplication ([('/(.*html)?', MainHandler)], debug=True)
-  util.run_wsgi_app (application)
+		path = os.path.join (os.path.dirname (__file__), q + '.html')
+		self.response.headers ['Content-Type'] = 'text/html'
+		self.response.out.write (template.render (path, {}))
 
-if __name__ == '__main__':
-  main ()
+# def main ():
+application = webapp.WSGIApplication ([('/(.*)', MainHandler)], debug=True)
+  # util.run_wsgi_app (application)
+
+# if __name__ == '__main__':
+#   main ()
+
+# # Copyright 2012 Digital Inspiration
+# # http://www.labnol.org/
+
+# import os
+# import webapp2
+# # from webapp2 import util
+# # from webapp2 import template
+# # from google.appengine.ext import webapp
+# # from google.appengine.ext.webapp import util
+# from google.appengine.ext.webapp import template
+
+# class MainHandler(webapp2.RequestHandler):
+#   def get (self, q):
+#     if q is None:
+#       q = 'index'
+
+#     path = os.path.join (os.path.dirname (__file__), q + '.html')
+#     self.response.headers ['Content-Type'] = 'text/html'
+#     self.response.write (path)
+
+
+# # def main ():
+# application = webapp2.WSGIApplication ([('/(.*)', MainHandler)], debug=True)
+#   # return application
+#   # util.run_wsgi_app (application)
+
+# # if __name__ == '__main__':
+# #   application = main ()
